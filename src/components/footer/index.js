48523c27f5cc807ee1187242a0a4eb28806adab0
Jesús Olazagoitia <jesus.designer@gmail.com>
2020-11-15 16:51:53 +0100

    add made by goiblas

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -7,29 +7,39 @@ import logo from "theme/isotipo-heliblocks-gray.svg";
 const Footer = () => {
   return (
     <Container as="footer">
-      <Flex py="6" alignItems="center" flexWrap="wrap">
-        <Image
-          src={logo}
-          width="28px"
-          height="auto"
-          opacity=".5"
-          mr="4"
-          transform="translateY(-3px)"
-        />
-        {/* <Text color="gray.500" fontSize="sm">
-          Made with ❤ by{" "}
-          <Link href="https://twitter.com/goiblas" isExternal>
-            @goiblas
-          </Link>
-        </Text> */}
-        <Text color="gray.500" fontSize="sm">
-          <Link as={RouterLink} to="/terms" mr="4" rel="nofollow">
-            Terms and Conditions
-          </Link>
-          <Link as={RouterLink} to="/privacy" rel="nofollow">
-            Privacy Policy
-          </Link>
-        </Text>
+      <Flex
+        py="6"
+        justifyContent="space-between"
+        alignItems="center"
+        flexWrap="wrap"
+      >
+        <Flex alignItems="center">
+          <Image
+            src={logo}
+            width="28px"
+            height="auto"
+            opacity=".5"
+            mr="4"
+            transform="translateY(-3px)"
+          />
+          <Text color="gray.500" fontSize="sm">
+            Made with ❤ by{" "}
+            <Link href="https://twitter.com/goiblas" isExternal>
+              @goiblas
+            </Link>
+          </Text>
+        </Flex>
+
+        <Flex alignItems="center">
+          <Text color="gray.500" fontSize="sm">
+            <Link as={RouterLink} to="/terms" mr="4" rel="nofollow">
+              Terms and Conditions
+            </Link>
+            <Link as={RouterLink} to="/privacy" rel="nofollow">
+              Privacy Policy
+            </Link>
+          </Text>
+        </Flex>
       </Flex>
     </Container>
   );
