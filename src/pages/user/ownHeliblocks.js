12de677a5b9cc5577e29fd7b763e3b79b2d72c23
Jesús Olazagoitia <jesus.designer@gmail.com>
2020-11-15 13:26:15 +0100

    fix profile heliblock not found

--- src/pages/user/ownHeliblocks.js
+++ src/pages/user/ownHeliblocks.js
@@ -1,43 +1,61 @@
 import React, { useState, useEffect, useCallback } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { CardOwner, SkeletonCard } from "components/card";
-import { sortByLastUpdate, fillHeliblocks, splitArrayByAttribute } from "./utilsProfile";
+import {
+  sortByLastUpdate,
+  fillHeliblocks,
+  splitArrayByAttribute,
+} from "./utilsProfile";
 
-const OwnHeliblocks = ({ ids } ) => {
-    const [ loading, setLoading ] = useState(true);
-    const [ hasError, setHasError ] = useState(false)
-    const [ heliblocks, setHeliblocks ] = useState([]);
+const OwnHeliblocks = ({ ids }) => {
+  const [loading, setLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+  const [heliblocks, setHeliblocks] = useState([]);
 
-    const arangeHeliblocks = useCallback((rawHeliblocks) => {
-        const heliblocksWithId = rawHeliblocks.map( (heliblock, index) => ({
-            ...heliblock,
-            id: ids[index]
-        }));
-        const [drafts, published ] = splitArrayByAttribute(heliblocksWithId, "draft");
-        setHeliblocks([...sortByLastUpdate(drafts), ...sortByLastUpdate(published)])
-    }, [ids])
+  const arangeHeliblocks = useCallback(
+    (rawHeliblocks) => {
+      const heliblocksWithId = rawHeliblocks
+        .map((heliblock, index) => ({ ...heliblock, id: ids[index] }))
+        .filter((heliblock) => !heliblock.notFound);
 
-    useEffect(() => {
-        fillHeliblocks(ids)
-            .then(arangeHeliblocks)
-            .catch(() => setHasError(true))
-            .finally(() => setLoading(false));
-    }, [ids, arangeHeliblocks]);
+      const [drafts, published] = splitArrayByAttribute(
+        heliblocksWithId,
+        "draft"
+      );
+      setHeliblocks([
+        ...sortByLastUpdate(drafts),
+        ...sortByLastUpdate(published),
+      ]);
+    },
+    [ids]
+  );
 
-    if(hasError) {
-        return null;
-    }
-    if(loading) {
-        return ids.map( id => <SkeletonCard key={id} />)
-    }
-    return heliblocks.map( heliblock => <CardOwner {...heliblock} key={heliblock.id} />)
-}
+  useEffect(() => {
+    fillHeliblocks(ids)
+      .then(arangeHeliblocks)
+      .catch((err) => {
+        console.log(err);
+        setHasError(true);
+      })
+      .finally(() => setLoading(false));
+  }, [ids, arangeHeliblocks]);
+
+  if (hasError) {
+    return null;
+  }
+  if (loading) {
+    return ids.map((id) => <SkeletonCard key={id} />);
+  }
+  return heliblocks.map((heliblock) => (
+    <CardOwner {...heliblock} key={heliblock.id} />
+  ));
+};
 
 export default OwnHeliblocks;
 
 OwnHeliblocks.propTypes = {
-    ids: PropTypes.array
-}
+  ids: PropTypes.array,
+};
 OwnHeliblocks.defaultProps = {
-    ids: []
-}
+  ids: [],
+};
